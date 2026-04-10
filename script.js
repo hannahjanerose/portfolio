9b660a74bcd0bcbb26ce241598bf881d7d516bf9
Hannah Rose <hannahroseseattle@gmail.com>
2026-04-10 13:00:43 -0700

    added functionality to project section, added project images and a project description for website, uploaded a higher res version of profile pic, adjusted formatting and cleaned up css for projct section

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function toggleDescription(button) {
+  const projectCard = button.closest('.details-container');
+  const description = projectCard.querySelector('.project-description');
+  description.classList.toggle('show');
+  if (description.classList.contains('show')) {
+    button.textContent = 'Show Less';
+  } else {
+    button.textContent = 'Learn More';
+  }
+}
